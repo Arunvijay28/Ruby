abeb9589ff75be01af63366253c0a74fc9a22e33
Arunvijay28 <arunvijaynov10@gamil.com>
2024-03-04 15:27:46 +0530

    new

---
+++ array.rb
@@ -0,0 +1,11 @@
+arr=Array(10..20)
+p arr
+puts "Arrays is #{arr.size}"
+
+puts"pushing into array using push"
+
+arr.push(22)
+
+p arr
+
+p "sum of array #{arr.sum}"
---
+++ switch.rb
@@ -0,0 +1,11 @@
+# grade = gets("enter ur grade:")
+grade = 80
+
+case 
+when grade >= 90
+    puts "A+"
+when grade >= 80 && grade <= 89
+    puts "A"
+else
+    puts "improve"
+end
